7b7b573a6ad9cf41f72bd2cd7c0f100a62e85f24
Luiz Pinheiro <luiz.pinheiro88@gmail.com>
2024-04-12 10:29:19 -0300

    denucia mod

--- script.js
+++ script.js
@@ -37,16 +37,16 @@ btnAbrirCamera.addEventListener("click", function() {
         return;
     }
 
-    // Verifique se há permissão para acessar a câmera
-    navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }) // Aqui definimos facingMode como "environment" para tentar acessar a câmera traseira
+    
+    navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }) 
         .then(function(stream) {
-            // Armazene a stream da câmera na variável cameraStream
+           
             cameraStream = stream;
 
-            // Exiba o modal
+           
             modal.style.display = "block";
 
-            // Exiba o elemento de vídeo para visualização da câmera
+           
             cameraPreview.srcObject = stream;
 
             console.log("Câmera aberta com sucesso!");
@@ -56,24 +56,24 @@ btnAbrirCamera.addEventListener("click", function() {
         });
 });
 
-// Evento de clique para o botão "Tirar Foto" dentro do modal
+
 btnTirarFoto.addEventListener("click", function() {
-    // Verifique se a stream da câmera está definida
+    
     if (!cameraStream) {
         alert("A câmera não foi aberta ainda!");
         return;
     }
 
-    // Crie um ImageCapture object usando o track de vídeo da stream da câmera
+    
     const imageCapturer = new ImageCapture(cameraStream.getVideoTracks()[0]);
 
-    // Tire a foto
+   
     imageCapturer.takePhoto()
         .then(function(blob) {
-            // Feche o modal
+           
             modal.style.display = "none";
 
-            // Exiba a mensagem "Foto arquivada" por 3 segundos
+            
             const mensagemArquivada = document.createElement("div");
             mensagemArquivada.textContent = "Foto arquivada";
             mensagemArquivada.classList.add("mensagem-arquivada");
@@ -87,24 +87,24 @@ btnTirarFoto.addEventListener("click", function() {
         });
 });
 
-// Função para consultar o CEP na página de denúncia
+
 async function consultarCEPDenuncia() {
     const inputCEP = document.getElementById("input-cep-denuncia").value;
 
     try {
         const data = await consultarCEP(inputCEP);
-        // Exibir o endereço buscado pelo site
+        
         const endereco = document.getElementById("endereco-buscado");
         endereco.textContent = `Endereço buscado: ${data.logradouro}, ${data.bairro}, ${data.localidade} - ${data.uf}`;
-        // Aqui você pode manipular os dados do CEP, como preencher campos de endereço na página de denúncia
+        
         console.log(data);
     } catch (error) {
-        // Trate o erro, como exibir uma mensagem para o usuário
+        
         console.error(error.message);
     }
 }
 
-// Evento de clique para o botão "Consultar CEP" na página de denúncia
+
 const btnConsultarCEPDenuncia = document.getElementById("btn-consultar-cep-denuncia");
 
 btnConsultarCEPDenuncia.addEventListener("click", consultarCEPDenuncia);
